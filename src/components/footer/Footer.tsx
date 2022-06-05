801a664eafde61f4ec8ee71a1652e06d0ea8a42a
99mac99 <maciekj99@gmail.com>
2022-06-05 23:47:46 +0200

    add footer skeleton

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,40 @@ export default function Footer() {
 	return (
 		<>
 			<div className={styles.mainBox}>
-                <p>dół</p>
-            </div>
+				<div className={styles.firstBox}>
+					<div>
+						<p>unimind.dao</p>
+						<p>Be where the change happens</p>
+					</div>
+					<div>
+						<div className={styles.firstBox}>
+							<div className={styles.partnersBox}>
+								<p>TWIT</p>
+							</div>
+							<div className={styles.partnersBox}>
+								<p>DISCO </p>
+							</div>
+							<div className={styles.partnersBox}>
+								<p>YT </p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className={styles.partnersMainBox}>
+					<div className={styles.partnersBox}>
+						<a>Contact</a>
+					</div>
+					<div className={styles.partnersBox}>
+						<a>Webtrzy.xyz</a>
+					</div>
+					<div className={styles.partnersBox}>
+						<a>NFTPolska</a>
+					</div>
+				</div>
+				<div className={styles.endBox}>
+					<p>2022 unimind.dao</p>
+				</div>
+			</div>
 		</>
 	);
 }
