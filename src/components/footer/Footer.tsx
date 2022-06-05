7fb7a29731ccded052aa026ea05a4f3e3c8ac07f
99mac99 <maciekj99@gmail.com>
2022-06-06 00:31:59 +0200

    add divided elements

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,42 +1,19 @@
 import styles from './Footer.module.css';
+// import { TwitterIcon, YouTubeIcon } from '@mui/icons-material';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FirstBox from './FirstBox';
+import PartnersBox from './PartnersBox';
+import EndBox from './EndBox'
+
+
+
 export default function Footer() {
 	return (
-		<>
-			<div className={styles.mainBox}>
-				<div className={styles.firstBox}>
-					<div>
-						<p>unimind.dao</p>
-						<p>Be where the change happens</p>
-					</div>
-					<div>
-						<div className={styles.firstBox}>
-							<div className={styles.partnersBox}>
-								<p>TWIT</p>
-							</div>
-							<div className={styles.partnersBox}>
-								<p>DISCO </p>
-							</div>
-							<div className={styles.partnersBox}>
-								<p>YT </p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className={styles.partnersMainBox}>
-					<div className={styles.partnersBox}>
-						<a>Contact</a>
-					</div>
-					<div className={styles.partnersBox}>
-						<a>Webtrzy.xyz</a>
-					</div>
-					<div className={styles.partnersBox}>
-						<a>NFTPolska</a>
-					</div>
-				</div>
-				<div className={styles.endBox}>
-					<p>2022 unimind.dao</p>
-				</div>
-			</div>
-		</>
+		<div className={styles.mainBox}>
+			<FirstBox />
+            <PartnersBox />
+            <EndBox />
+		</div>
 	);
 }
